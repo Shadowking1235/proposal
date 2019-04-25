a2a6bf9a1579c18e8d04d2cb1f96b8dbe2e6fb07
Shadowking1235 <43701529+Shadowking1235@users.noreply.github.com>
2019-04-25 10:23:44 +0400

    Add files via upload

---
+++ Proposal.py
@@ -0,0 +1,13 @@
+"""""""""
+My first project will consist of a Hospital appointment management project
+1. https://github.com/bhaviya08/Hospital-management-python-/blob/master/appointment.py
+*My second project which also happens to be my favourite will consist of a simple Inventory Manaement project to understand queue and parallel programing.
+2.https://github.com/dMilluh/Python-I-Inventory-Management-Project/blob/master/app.py
+3.My third project consist of a Password_Generator to understand more of practical programming.
+ https://github.com/BlucyBlue/Python-password-generator/blob/master/password.py
+4. My third project is a chess game  
+game in python to understanding more about the chess algorithm
+https://gist.github.com/rsheldiii/2993225
+5. My last project consist if a Ludo board game i also choose this project to understand more about about board games 
+https://github.com/prakashbhatt1/ludo-game-in-python/edit/master/snake.py
+"""""""""
